0ee7c31e6e210db811c9153b4fe349d911c022ba
zhangchunsheng <zhangchunsheng423@gmail.com>
2014-05-04 01:16:41 +0800

    add express demo

---
+++ node/demos/demo/app.js
@@ -0,0 +1,51 @@
+/**
+ * Created by zhang on 5/4/14.
+ */
+var express = require('express');
+var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var app = express();
+
+// 设定port变量，意为访问端口
+app.set('port', process.env.PORT || 3000);
+
+// 设定views变量，意为视图存放的目录
+app.set('views', path.join(__dirname, 'views'));
+
+// 设定view engine变量，意为网页模板引擎
+app.set('view engine', 'jade');
+
+//app.use(express.favicon());
+//app.use(express.logger('dev'));
+//app.use(express.bodyParser());
+//app.use(express.methodOverride());
+//app.use(app.router);
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+
+// 设定静态文件目录，比如本地文件
+// 目录为demo/public/images，访问
+// 网址则显示为http://localhost:3000/images
+app.use(express.static(path.join(__dirname, 'public')));
+
+/*app.get('/', function(req, res) {
+    res.send('Hello World');
+});*/
+
+app.get('/', function(req, res){
+    var body = 'Hello World';
+    res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Content-Length', body.length);
+    res.end(body);
+});
+
+app.listen(app.get('port'));
+
+console.log("server start at 3000...");
